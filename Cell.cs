af218b8e14685ae487d6356e890d4574401bc4d4
Richard Boyd <34223701+RichardvBoyd2@users.noreply.github.com>
2018-12-13 16:37:51 -0700

    Milestone 1
    .cs files for Milestone 1 of CST 227
    Cell and Board classes, and a driver class to run the classes in a console app

---
+++ Cell.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CST227Milestone1
+{
+    class Cell
+    {
+        public int row { get; set; } = -1;
+        public int col { get; set; } = -1;
+        public bool visited { get; set; } = false;
+        public bool live { get; set; } = false;
+        public int liveneighbors { get; set; } = 0;
+
+        public Cell() {}        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CST227Milestone1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool success = false;
+            while (success == false)
+            {
+                Console.WriteLine("Enter an integer to determine the size of each side of the square:");
+                if (Int32.TryParse(Console.ReadLine(), out int size))
+                {
+                    success = true;
+                    Board board = new Board(size);
+                    board.Activate();
+                    board.NeighborCount();
+                    Console.WriteLine();
+                    Console.WriteLine("Here is your " + size + " X " + size + " game board");
+                    board.Display();
+                    Console.WriteLine();
+                    Console.WriteLine("Enter 'Y' to make another, or hit enter to exit");
+                    string again = Console.ReadLine();
+                    if (again == "Y" || again == "y")
+                    {
+                        success = false;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Could not recognize input as an integer.");
+                }                
+            }            
+        }
+    }
+}
